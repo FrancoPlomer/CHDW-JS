0e3bfd702312e4ce29b2c55b08a1771dade193ca
PlomerFranco <franko.plomer@gmail.com>
2021-06-10 16:26:39 -0300

    Mejorando el codigo js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,109 @@
-let element1 = document.getElementById("elemento1")
-let element2 = document.getElementById("elemento2")
-let element3 = document.getElementById("elemento3")
-let element4 = document.getElementById("elemento4")
-let element5 = document.getElementById("elemento5")
-let element6 = document.getElementById("elemento6")
-let element7 = document.getElementById("elemento7")
-let element8 = document.getElementById("elemento8")
+const elementos =[{id:"element1", src:"http://placehold.it/350x280/444?text=1"},
+{id:"element2", src:"http://placehold.it/350x280/444?text=2"},
+{id:"element3", src:"http://placehold.it/350x280/444?text=3"},
+{id:"element4", src:"http://placehold.it/350x280/444?text=4"}
+];
+const elementosSegundaUnidad = [{id:"element5", src:"http://placehold.it/350x280/444?text=5"},
+{id:"element6", src:"http://placehold.it/350x280/444?text=6"},
+{id:"element7", src:"http://placehold.it/350x280/444?text=7"},
+{id:"element8", src:"http://placehold.it/350x280/444?text=8"}];
+
+let primerContenedor = document.getElementById("primerCarrousel");
+let segundoContenedor = document.getElementById("segundoCarrousel");
+
+for(let elemento of elementos)
+{
+    let container = document.createElement("div")
+    container.className = "col-3 float-left"
+    container.innerHTML = `<button id="${elemento.id}" data-bs-toggle="modal"
+    data-bs-target="#exampleModal"><img class="img-fluid"
+        src="${elemento.src}"></button>`
+    primerContenedor.appendChild(container);
+}
+
+for(let elementoAd of elementosSegundaUnidad)
+{
+    let container = document.createElement("div")
+    container.className = "col-3 float-left"
+    container.innerHTML = `<button id="${elementoAd.id}" data-bs-toggle="modal"
+    data-bs-target="#exampleModal"><img class="img-fluid"
+        src="${elementoAd.src}"></button>`
+        segundoContenedor.appendChild(container);
+}
+
 let save = document.getElementById("save")
-element1.addEventListener("click", clickeaste1)
-element2.addEventListener("click", clickeaste2)
-element3.addEventListener("click", clickeaste3)
-element4.addEventListener("click", clickeaste4)
-element5.addEventListener("click", clickeaste5)
-element6.addEventListener("click", clickeaste6)
-element7.addEventListener("click", clickeaste7)
-element8.addEventListener("click", clickeaste8)
+element1.addEventListener("click", ()=> {
+    let nombreModal = document.getElementsByClassName("Descripcion");
+    let precioModal = document.getElementsByClassName("precio");
+    let ImagenModal = document.getElementsByClassName("imagenModal");
+    nombreModal[0].textContent = primerElemento.nombre;
+    precioModal[0].textContent = primerElemento.precio;
+    ImagenModal[0].src = primerElemento.source;
+})
+element2.addEventListener("click", ()=> {
+    let nombreModal = document.getElementsByClassName("Descripcion");
+    let precioModal = document.getElementsByClassName("precio");
+    let ImagenModal = document.getElementsByClassName("imagenModal");
+    let save = document.getElementsByClassName("save")
+    nombreModal[0].textContent = segundoElemento.nombre;
+    precioModal[0].textContent = segundoElemento.precio;
+    ImagenModal[0].src = segundoElemento.source;
+})
+element3.addEventListener("click", ()=> {
+    let nombreModal = document.getElementsByClassName("Descripcion");
+    let precioModal = document.getElementsByClassName("precio");
+    let ImagenModal = document.getElementsByClassName("imagenModal");
+    let save = document.getElementsByClassName("save")
+    nombreModal[0].textContent = tercerElemento.nombre;
+    precioModal[0].textContent = tercerElemento.precio;
+    ImagenModal[0].src = tercerElemento.source;
+})
+element4.addEventListener("click", ()=> {
+    let nombreModal = document.getElementsByClassName("Descripcion");
+    let precioModal = document.getElementsByClassName("precio");
+    let ImagenModal = document.getElementsByClassName("imagenModal");
+    let save = document.getElementsByClassName("save")
+    nombreModal[0].textContent = cuartoElemento.nombre;
+    precioModal[0].textContent = cuartoElemento.precio;
+    ImagenModal[0].src = cuartoElemento.source;
+})
+element5.addEventListener("click", ()=> {
+    let nombreModal = document.getElementsByClassName("Descripcion");
+    let precioModal = document.getElementsByClassName("precio");
+    let ImagenModal = document.getElementsByClassName("imagenModal");
+    let save = document.getElementsByClassName("save")
+    nombreModal[0].textContent = quintoElemento.nombre;
+    precioModal[0].textContent = quintoElemento.precio;
+    ImagenModal[0].src = quintoElemento.source;
+})
+element6.addEventListener("click", ()=> {
+    let nombreModal = document.getElementsByClassName("Descripcion");
+    let precioModal = document.getElementsByClassName("precio");
+    let ImagenModal = document.getElementsByClassName("imagenModal");
+    let save = document.getElementsByClassName("save")
+    nombreModal[0].textContent = sextoElemento.nombre;
+    precioModal[0].textContent = sextoElemento.precio;
+    ImagenModal[0].src = sextoElemento.source;
+})
+element7.addEventListener("click", ()=> {
+    let nombreModal = document.getElementsByClassName("Descripcion");
+    let precioModal = document.getElementsByClassName("precio");
+    let ImagenModal = document.getElementsByClassName("imagenModal");
+    let save = document.getElementsByClassName("save")
+    nombreModal[0].textContent = septimoElemento.nombre;
+    precioModal[0].textContent = septimoElemento.precio;
+    ImagenModal[0].src = septimoElemento.source;
+}
+)
+element8.addEventListener("click", ()=> {
+    let nombreModal = document.getElementsByClassName("Descripcion");
+    let precioModal = document.getElementsByClassName("precio");
+    let ImagenModal = document.getElementsByClassName("imagenModal");
+    let save = document.getElementsByClassName("save")
+    nombreModal[0].textContent = octavoElemento.nombre;
+    precioModal[0].textContent = octavoElemento.precio;
+    ImagenModal[0].src = octavoElemento.source;
+})
 save.addEventListener("click", guardarStorage)
 const primerElemento = {
     nombre: "Buzo", 
@@ -57,85 +146,6 @@ const octavoElemento = {
     source:"http://placehold.it/350x280/222/fff?text=8"
 };
 
-function clickeaste1() {
-    let nombreModal = document.getElementsByClassName("Descripcion");
-    let precioModal = document.getElementsByClassName("precio");
-    let ImagenModal = document.getElementsByClassName("imagenModal");
-    nombreModal[0].textContent = primerElemento.nombre;
-    precioModal[0].textContent = primerElemento.precio;
-    ImagenModal[0].src = primerElemento.source;
-}
-
-function clickeaste2() {
-    let nombreModal = document.getElementsByClassName("Descripcion");
-    let precioModal = document.getElementsByClassName("precio");
-    let ImagenModal = document.getElementsByClassName("imagenModal");
-    let save = document.getElementsByClassName("save")
-    nombreModal[0].textContent = segundoElemento.nombre;
-    precioModal[0].textContent = segundoElemento.precio;
-    ImagenModal[0].src = segundoElemento.source;
-}
-
-function clickeaste3() {
-    let nombreModal = document.getElementsByClassName("Descripcion");
-    let precioModal = document.getElementsByClassName("precio");
-    let ImagenModal = document.getElementsByClassName("imagenModal");
-    let save = document.getElementsByClassName("save")
-    nombreModal[0].textContent = tercerElemento.nombre;
-    precioModal[0].textContent = tercerElemento.precio;
-    ImagenModal[0].src = tercerElemento.source;
-}
-
-function clickeaste4() {
-    let nombreModal = document.getElementsByClassName("Descripcion");
-    let precioModal = document.getElementsByClassName("precio");
-    let ImagenModal = document.getElementsByClassName("imagenModal");
-    let save = document.getElementsByClassName("save")
-    nombreModal[0].textContent = cuartoElemento.nombre;
-    precioModal[0].textContent = cuartoElemento.precio;
-    ImagenModal[0].src = cuartoElemento.source;
-}
-
-function clickeaste5() {
-    let nombreModal = document.getElementsByClassName("Descripcion");
-    let precioModal = document.getElementsByClassName("precio");
-    let ImagenModal = document.getElementsByClassName("imagenModal");
-    let save = document.getElementsByClassName("save")
-    nombreModal[0].textContent = quintoElemento.nombre;
-    precioModal[0].textContent = quintoElemento.precio;
-    ImagenModal[0].src = quintoElemento.source;
-}
-
-function clickeaste6() {
-    let nombreModal = document.getElementsByClassName("Descripcion");
-    let precioModal = document.getElementsByClassName("precio");
-    let ImagenModal = document.getElementsByClassName("imagenModal");
-    let save = document.getElementsByClassName("save")
-    nombreModal[0].textContent = sextoElemento.nombre;
-    precioModal[0].textContent = sextoElemento.precio;
-    ImagenModal[0].src = sextoElemento.source;
-}
-
-function clickeaste7() {
-    let nombreModal = document.getElementsByClassName("Descripcion");
-    let precioModal = document.getElementsByClassName("precio");
-    let ImagenModal = document.getElementsByClassName("imagenModal");
-    let save = document.getElementsByClassName("save")
-    nombreModal[0].textContent = septimoElemento.nombre;
-    precioModal[0].textContent = septimoElemento.precio;
-    ImagenModal[0].src = septimoElemento.source;
-}
-
-function clickeaste8() {
-    let nombreModal = document.getElementsByClassName("Descripcion");
-    let precioModal = document.getElementsByClassName("precio");
-    let ImagenModal = document.getElementsByClassName("imagenModal");
-    let save = document.getElementsByClassName("save")
-    nombreModal[0].textContent = octavoElemento.nombre;
-    precioModal[0].textContent = octavoElemento.precio;
-    ImagenModal[0].src = octavoElemento.source;
-}
-
 function guardarStorage()
 {
     
@@ -160,6 +170,5 @@ function guardarStorage()
         objeto.precio = precioLista * objeto.cantidad;
         localStorage.setItem(nombreModal[0].textContent, JSON.stringify(objeto))
     }
-
 
 }
